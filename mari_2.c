a0b5b065caa932e5a3c8e27851d69107cd6e1429
36c <mari_36is@hotmail.com>
2015-12-07 21:47:14 -0600

    maty

---
+++ mari_2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void funcion();
+int suma(int a, int b)
+
+{
+	int j;
+	printf("j=%d\n",j);
+}
+{
+	int res;
+	res=a+b; printf("funcion i=%d\n");
+	return res;
+}
+int main()
+{
+	int res;
+	res=suma(2,5);
+	printf("%d\n",res);
+	
+	int j;
+	funcion();
+	j=10;
+	funcion();
+}
+
+
